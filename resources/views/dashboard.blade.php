66a66745add63c99afc8f8d46492d9a73afe6bb2
annalogues <levy35817@gmail.com>
2024-11-30 23:43:09 +0100

    ajout du footer et stylisation du footer dans le dossier css/particles.css

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -68,6 +68,21 @@
     </div>
   </div>
 
+  <div class="container">
+    <footer class="py-3 my-4 fixed">
+      <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-white">Home</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-white">Features</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-white">Pricing</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-white">About</a></li>
+      </ul>
+      <p class="text-center text-white code-line">
+        &copy;2024 <code>$dev= "Levy | DEV";echo $dev;</code>
+      </p>      
+    </footer>
+  </div>
+
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 
   <!-- scripts -->
